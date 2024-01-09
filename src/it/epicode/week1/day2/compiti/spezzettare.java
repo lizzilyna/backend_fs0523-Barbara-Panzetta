22fa96a8d6b9cd285cbc3037a717b076ca61ab2c
lizzlyna <barbara.panzetta@gmail.com>
2024-01-09 18:03:57 +0100

    compiti 2 last commit

--- src/it/epicode/week1/day2/compiti/spezzettare.java
+++ src/it/epicode/week1/day2/compiti/spezzettare.java
@@ -1,13 +1,29 @@
-/*package it.epicode.week1.day2.compiti;
+package it.epicode.week1.day2.compiti;
 
 import java.util.Scanner;
 
 public class spezzettare {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Dammi una parola");
-        String parola =scanner.nextLine();
+       ;
+        String parola;
 
+
+                do{
+                    System.out.println("Dammi una parola");
+                parola =scanner.nextLine();
+
+               for (int i=0; i<parola.length(); i++)
+
+               {
+                   System.out.print(parola.charAt(i));
+                   if (i!=parola.length()-1)
+                       System.out.println(",");
+
+               }
+                }
+        while (!parola.equals(":q"));}}
+/*
     }
 public static void spezzettare (String vocabolo)
 {do {
